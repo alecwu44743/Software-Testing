bbb26182b8d7f8675af26d3caef53b0eb7962562
alecwu44743 <alec.wu44743@gmail.com>
2023-10-13 02:54:28 +0800

    [HW01] triangle/input using try-catch, exception

--- HW01/src/main/java/org/example/Main.java
+++ HW01/src/main/java/org/example/Main.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void numberCheck(double num) throws Exception {
+        if (num < 0) {
+            throw new Exception("Side a cannot be negative");
+        }
+        else if (num == 0) {
+            throw new Exception("Side a cannot be zero");
+        }
+        else if (Double.isNaN(num)) {
+            throw new NumberFormatException("Not a valid number");
+        }
+    }
+
     // check if a triangle is equilateral, isosceles, or scalene
     int checkTriangle(double a, double b, double c) {
         if (a == b && b == c) {
@@ -18,22 +30,34 @@ public class Main {
 
     public static void main(String[] args) {
         // input three side lengths of a triangle
-        double a, b, c;
+        double a = 0, b = 0, c = 0;
+        boolean isValid = true;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter three side lengths of a triangle: ");
-        a = scanner.nextDouble();
-        b = scanner.nextDouble();
-        c = scanner.nextDouble();
+        System.out.print("Triangle Checker\n");
+        try{
+            System.out.print("Enter side a: "); a = scanner.nextDouble();
+            numberCheck(a); // check if a is valid
 
-        int result = new Main().checkTriangle(a, b, c);
-        if (result == 1) {
-            System.out.println("Equilateral triangle");
-        } else if (result == 2) {
-            System.out.println("Isosceles triangle");
-        } else {
-            System.out.println("Scalene triangle");
+            System.out.print("Enter side b: "); b = scanner.nextDouble();
+            numberCheck(b); // check if b is valid
+
+            System.out.print("Enter side c: "); c = scanner.nextDouble();
+            numberCheck(c); // check if c is valid
+
+            int res = new Main().checkTriangle(a, b, c);
+        }catch (Exception e){
+            isValid = false;
+            System.out.println(e.getMessage());
+        }catch (java.util.InputMismatchException e) {
+            System.err.println("Invalid input. Please enter a valid number.");
+        }
+        finally {
+            scanner.close();
         }
 
+        if (isValid) {
+            System.out.println("The three sides of the triangle are " + a + ", " + b + ", and " + c);
+        }
     }
 }
