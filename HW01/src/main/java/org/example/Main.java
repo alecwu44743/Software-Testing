c497eba91db00205f0eba2cb4c489cfc158383a4
alecwu44743 <alec.wu44743@gmail.com>
2023-10-14 00:52:47 +0800

    [HW01] triangle/done input, exception, testing

--- HW01/src/main/java/org/example/Main.java
+++ HW01/src/main/java/org/example/Main.java
@@ -12,19 +12,16 @@ public class Main {
         else if (num == 0) {
             throw new Exception("Side a cannot be zero");
         }
-        else if (Double.isNaN(num)) {
-            throw new NumberFormatException("Not a valid number");
-        }
     }
 
     // check if a triangle is equilateral, isosceles, or scalene
     int checkTriangle(double a, double b, double c) {
         if (a == b && b == c) {
-            return 1;
+            return 1; // equilateral triangle
         } else if (a == b || b == c || a == c) {
-            return 2;
+            return 2; // isosceles triangle
         } else {
-            return 3;
+            return 3; // scalene triangle
         }
     }
 
@@ -36,27 +33,65 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Triangle Checker\n");
-        try{
-            System.out.print("Enter side a: "); a = scanner.nextDouble();
-            numberCheck(a); // check if a is valid
 
-            System.out.print("Enter side b: "); b = scanner.nextDouble();
-            numberCheck(b); // check if b is valid
-
-            System.out.print("Enter side c: "); c = scanner.nextDouble();
-            numberCheck(c); // check if c is valid
-
-            int res = new Main().checkTriangle(a, b, c);
-        }catch (Exception e){
-            isValid = false;
-            System.out.println(e.getMessage());
+        // input side a
+        while (true) {
+            System.out.print("Enter side a: ");
+            try {
+                a = Double.parseDouble(scanner.next());
+                numberCheck(a); // check if a is valid
+                break; // will only get to here if input was a double
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e){
+                isValid = false;
+                System.out.println(e.getMessage());
+            }
         }
-        finally {
-            scanner.close();
+
+        // input side b
+        while (true) {
+            System.out.print("Enter side b: ");
+            try {
+                b = Double.parseDouble(scanner.next());
+                numberCheck(b); // check if b is valid
+                break; // will only get to here if input was a double
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e){
+                isValid = false;
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // input side c
+        while (true) {
+            System.out.print("Enter side c: ");
+            try {
+                c = Double.parseDouble(scanner.next());
+                numberCheck(c); // check if c is valid
+                break; // will only get to here if input was a double
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e){
+                isValid = false;
+                System.out.println(e.getMessage());
+            }
         }
 
         if (isValid) {
             System.out.println("The three sides of the triangle are " + a + ", " + b + ", and " + c);
+
+            // check the type of triangle, and print the result
+            Main main = new Main();
+            int result = main.checkTriangle(a, b, c);
+            if (result == 1) {
+                System.out.println("This is an equilateral triangle");
+            } else if (result == 2) {
+                System.out.println("This is an isosceles triangle");
+            } else {
+                System.out.println("This is a scalene triangle");
+            }
         }
     }
 }
--- HW01/src/test/java/org/example/MainTest.java
+++ HW01/src/test/java/org/example/MainTest.java
@@ -3,29 +3,86 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 
 
 class MainTest {
+    // print the pass or fail message
+    public static void printPassOrFail(
+            int expectedValue, int actualValue, int caseNumber,
+            int sideA, int sideB, int sideC) {
+        if (expectedValue == actualValue) { // if the test passes, print pass
+            System.out.println("Case #" + caseNumber + ": pass");
+        } else { // if the test fails, print fail
+            System.out.println("Case #" + caseNumber + ": fail");
+        }
+
+        // print the expected and actual values, and the three sides
+        System.out.println("side a: " + sideA + ", side b: " + sideB + ", side c: " + sideC);
+        System.out.println("Expected: " + expectedValue + ", Actual: " + actualValue);
+        System.out.println();
+    }
 
     Main main = new Main();
 
-    @BeforeEach
+    @BeforeEach // To initialize the test
     void init() {
         main = new Main();
 //        System.out.println("init");
-        System.out.println("Here is before each");
+        System.out.println("[>] Init MainTest");
     }
 
     @org.junit.jupiter.api.Test
     void checkTriangle() {
-        System.out.println("Here is plus test");
+        System.out.println("Here is checkTriangle test\n");
+        System.out.println("1 -> equilateral triangle");
+        System.out.println("2 -> isosceles triangle");
+        System.out.println("3 -> scalene triangle");
+        System.out.println();
+
         int actualValue = main.checkTriangle(6, 6, 3);
         int expectedValue = 2;
 
         assertEquals(expectedValue, actualValue);
+
+        assertEquals(1, main.checkTriangle(6, 6, 6), "Should be equilateral triangle");
+        // print pass message if the test passes
+        printPassOrFail(1, main.checkTriangle(6, 6, 6), 1, 6, 6, 6);
+
+        assertEquals(2, main.checkTriangle(6, 6, 3), "Should be isosceles triangle");
+        printPassOrFail(2, main.checkTriangle(6, 6, 3), 2, 6, 6, 3);
+
+        assertEquals(3, main.checkTriangle(6, 5, 3), "Should be scalene triangle");
+        printPassOrFail(3, main.checkTriangle(6, 5, 3), 3, 6, 5, 3);
+
+        assertEquals(3, main.checkTriangle(6, 3, 5), "Should be scalene triangle");
+        printPassOrFail(3, main.checkTriangle(6, 3, 5), 4, 6, 3, 5);
+
+        assertEquals(1, main.checkTriangle(6, 6, 6), "Should be equilateral triangle");
+        printPassOrFail(1, main.checkTriangle(6, 6, 6), 5, 6, 6, 6);
+
+        assertEquals(2, main.checkTriangle(6, 6, 3), "Should be isosceles triangle");
+        printPassOrFail(2, main.checkTriangle(6, 6, 3), 6, 6, 6, 3);
     }
 
     @org.junit.jupiter.api.Test
     void main() {
+        System.out.println("Here is main test for exception\n");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            main.numberCheck(-1);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is -1, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            main.numberCheck(0);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Input is 0, " + "Exception message: " + exception.getMessage());
     }
 }
