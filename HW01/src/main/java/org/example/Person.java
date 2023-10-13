c62df64dd601be8313342d7b69ec4ce53002f6df
alecwu44743 <alec.wu44743@gmail.com>
2023-10-14 04:02:38 +0800

    [HW01] Person/testing done

--- HW01/src/main/java/org/example/Person.java
+++ HW01/src/main/java/org/example/Person.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class Person {
     String name;
     int yearOfBirth;
@@ -11,25 +14,223 @@ public class Person {
     double weight;
     double bmi;
 
-    public Person(String name, int yearOfBirth, int monthOfBirth, int dayOfBirth, double height, double weight) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.monthOfBirth = monthOfBirth;
-        this.dayOfBirth = dayOfBirth;
+    public Person(String _name, int _yearOfBirth, int _monthOfBirth, int _dayOfBirth, double _height, double _weight) {
+        this.name = _name;
+        this.yearOfBirth = _yearOfBirth;
+        this.monthOfBirth = _monthOfBirth;
+        this.dayOfBirth = _dayOfBirth;
 
         LocalDate currentDate = LocalDate.now();
-        this.age = currentDate.getYear() - yearOfBirth;
+        this.age = currentDate.getYear() - this.yearOfBirth;
 
-        this.height = height;
-        this.weight = weight;
+        this.height = _height;
+        this.weight = _weight;
         this.bmi = calculateBMI();
+    }
+
+    public Person(){
+
     }
 
     private double calculateBMI() {
         return this.weight / ((this.height/100) * (this.height/100));
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getBMI() {
+        return this.bmi;
+    }
+
+    public String getBirthDate() {
+        return this.monthOfBirth + "/" + this.dayOfBirth + "/" + this.yearOfBirth;
+    }
+
+    public String showMyInfo() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        String correctBmi = df.format(this.bmi);
+        String correctHeight = df.format(this.height);
+        String correctWeight = df.format(this.weight);
+
+        return "\nName: " + this.name + "\n" +
+                "Age: " + this.age + "\n" +
+                "Height: " + correctHeight + " cm\n" +
+                "Weight: " + correctWeight + " kg\n" +
+                "BMI: " + correctBmi + "\n" +
+                "Birth date: " + this.monthOfBirth + "/" + this.dayOfBirth + "/" + this.yearOfBirth;
+    }
+
+    public static void nameCheck(String name) throws Exception {
+        if (name.length() < 1 || name.length() > 100) {
+            throw new Exception("Name cannot be less than 1 or greater than 100 characters");
+        }
+    }
+
+    public static void yearCheck(int year) throws Exception {
+        LocalDate currentDate = LocalDate.now();
+        if (year < 1850 || year > currentDate.getYear()) {
+            throw new Exception("Year of birth cannot be less than 1850 or greater than current year " + currentDate.getYear());
+        }
+    }
+
+    public static void monthCheck(int month, int year) throws Exception {
+        LocalDate currentDate = LocalDate.now();
+        if (month < 1 || month > 12) {
+            throw new Exception("Month of birth cannot be less than 1 or greater than 12");
+        }
+        else{
+            if (year == currentDate.getYear()) {
+                if (month > currentDate.getMonthValue()) {
+                    throw new Exception("Month of birth cannot be greater than current month");
+                }
+            }
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void dayCheck(int month, int day, int year) throws Exception {
+        LocalDate currentDate = LocalDate.now();
+        if (day < 1 || day > 31) {
+            throw new Exception("Day of birth cannot be less than 1 or greater than 31");
+        }
+        else{
+            if (year == currentDate.getYear()) {
+                if (month == currentDate.getMonthValue()) {
+                    if (day > currentDate.getDayOfMonth()) {
+                        throw new Exception("Day of birth cannot be greater than current day");
+                    }
+                }
+            }
+            else{
+                if (month == 2) {
+                    if (isLeapYear(year)) {
+                        if (day > 29) {
+                            throw new Exception("Day of birth cannot be greater than 29");
+                        }
+                    }
+                    else{
+                        if (day > 28) {
+                            throw new Exception("Day of birth cannot be greater than 28");
+                        }
+                    }
+                }
+                else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                    if (day > 30) {
+                        throw new Exception("Day of birth cannot be greater than 30");
+                    }
+                }
+            }
+        }
+    }
+
+    public static void heightCheck(double height) throws Exception {
+        if (height < 10 || height > 300) {
+            throw new Exception("Height cannot be less than 0 or greater than 300\n" + "Please use cm");
+        }
+    }
+
+    public static void weightCheck(double weight) throws Exception {
+        if (weight <= 0 || weight > 500) {
+            throw new Exception("Weight cannot be less than 0 or greater than 500\n" + "Please use kg");
+        }
+    }
+
+
     public static void main(String[] args) {
-        // to do
+        String _name;
+        int _yearOfBirth, _monthOfBirth, _dayOfBirth;
+        double _height, _weight;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            try {
+                System.out.print("Enter name: ");
+                _name = scanner.nextLine();
+                nameCheck(_name);
+                break;
+            } catch (Exception ignore) {
+                System.out.println("Invalid input");
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter year of birth: ");
+                _yearOfBirth  = Integer.parseInt(scanner.nextLine());
+                yearCheck(_yearOfBirth);
+                break;
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter month of birth: ");
+                _monthOfBirth = Integer.parseInt(scanner.nextLine());
+                monthCheck(_monthOfBirth, _yearOfBirth);
+                break;
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter day of birth: ");
+                _dayOfBirth = Integer.parseInt(scanner.nextLine());
+                dayCheck(_monthOfBirth, _dayOfBirth, _yearOfBirth);
+                break;
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter height(cm): ");
+                _height = Double.parseDouble(scanner.next());
+                heightCheck(_height);
+                break;
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter weight(kg): ");
+                _weight = Double.parseDouble(scanner.next());
+                weightCheck(_weight);
+                break;
+            } catch (NumberFormatException ignore) {
+                System.out.println("Invalid input");
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        Person person = new Person(_name, (int)_yearOfBirth, (int)_monthOfBirth, (int)_dayOfBirth, _height, _weight);
+
+        System.out.println(person.showMyInfo());
     }
 }
--- HW01/src/test/java/org/example/PersonTest.java
+++ HW01/src/test/java/org/example/PersonTest.java
@@ -1,4 +1,298 @@
+package org.example;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+    public void printPassOrFail_name(String expectedValue, String actualValue, int caseNumber){
+        if (expectedValue == actualValue) { // if the test passes, print pass
+            System.out.println("Case #" + caseNumber + ": pass");
+        } else { // if the test fails, print fail
+            System.out.println("Case #" + caseNumber + ": fail");
+        }
+
+        System.out.println("Expected: " + expectedValue + ", Actual: " + actualValue);
+        System.out.println();
+    }
+
+    Person person = new Person();
+    Person personA = new Person();
+    Person personB = new Person();
+    Person personC = new Person();
+    Person personD = new Person();
+    Person personE = new Person();
+
+    @BeforeEach
+        // To initialize the test
+    void init() {
+        person = new Person("Nguyen Van A", 2000, 12, 31, 170, 60);
+        personA = new Person("Alec Wu", 2023, 1, 21, 170, 60);
+        personB = new Person("", 2000, 12, 31, 170, 60);
+        personC = new Person("Emily Chen", 1991, 5, 32, 169, 50);
+        personD = new Person("Elle CX", 1993, 2, 29, 168, 50);
+        personE = new Person("Ashley CA", 1992, 2, 29, 168, 50);
+
+//        System.out.println("init");
+        System.out.println("[>] Init MainTest\n");
+    }
+
+    @Test
+    void getName() {
+        System.out.println("[+] Here is getName test");
+        System.out.println("This test initially bypasses judgment and solely assesses whether someone can retrieve the name");
+        assertEquals("Nguyen Van A", person.getName());
+        assertEquals("Alec Wu", personA.getName());
+        assertEquals("", personB.getName());
+        assertEquals("Emily Chen", personC.getName());
+        assertEquals("Elle CX", personD.getName());
+        assertEquals("Ashley CA", personE.getName());
+
+        System.out.println("[v] getName test passed");
+    }
+
+    @Test
+    void getAge() {
+        System.out.println("[+] Here is getAge test");
+        System.out.println("This test initially bypasses judgment and solely assesses whether someone can retrieve the age");
+
+        LocalDate currentDate = LocalDate.now();
+        assertEquals(currentDate.getYear() - 2000, person.getAge());
+        assertEquals(currentDate.getYear() - 2023, personA.getAge());
+        assertEquals(currentDate.getYear() - 2000, personB.getAge());
+        assertEquals(currentDate.getYear() - 1991, personC.getAge());
+        assertEquals(currentDate.getYear() - 1993, personD.getAge());
+        assertEquals(currentDate.getYear() - 1992, personE.getAge());
+
+        System.out.println("[v] getAge test passed");
+    }
+
+    @Test
+    void getBMI() {
+        System.out.println("[+] Here is getBMI test");
+        System.out.println("This test initially bypasses judgment and solely assesses whether someone can retrieve the BMI");
+
+        assertEquals(20.761245674740486, person.getBMI());
+        assertEquals(20.761245674740486, personA.getBMI());
+        assertEquals(20.761245674740486, personB.getBMI());
+        assertEquals(17.506389832288786, personC.getBMI());
+        assertEquals(17.71541950113379, personD.getBMI());
+        assertEquals(17.71541950113379, personE.getBMI());
+
+        System.out.println("[v] getBMI test passed");
+    }
+
+    @Test
+    void getBirthDate() {
+        System.out.println("[+] Here is getBirthDate test");
+        System.out.println("This test initially bypasses judgment and solely assesses whether someone can retrieve the birth date");
+
+        assertEquals("12/31/2000", person.getBirthDate());
+        assertEquals("1/21/2023", personA.getBirthDate());
+        assertEquals("12/31/2000", personB.getBirthDate());
+        assertEquals("5/32/1991", personC.getBirthDate());
+        assertEquals("2/29/1993", personD.getBirthDate());
+        assertEquals("2/29/1992", personE.getBirthDate());
+
+        System.out.println("[v] getBirthDate test passed");
+    }
+
+    @Test
+    void showMyInfo() {
+        System.out.println("[+] Here is showMyInfo test");
+        System.out.println("This test initially bypasses judgment and solely assesses whether someone can retrieve the information");
+
+        assertEquals("\nName: Nguyen Van A\n" +
+                "Age: 23\n" +
+                "Height: 170.00 cm\n" +
+                "Weight: 60.00 kg\n" +
+                "BMI: 20.76\n" +
+                "Birth date: 12/31/2000", person.showMyInfo());
+        assertEquals("\nName: Alec Wu\n" +
+                "Age: 0\n" +
+                "Height: 170.00 cm\n" +
+                "Weight: 60.00 kg\n" +
+                "BMI: 20.76\n" +
+                "Birth date: 1/21/2023", personA.showMyInfo());
+        assertEquals("\nName: \n" +
+                "Age: 23\n" +
+                "Height: 170.00 cm\n" +
+                "Weight: 60.00 kg\n" +
+                "BMI: 20.76\n" +
+                "Birth date: 12/31/2000", personB.showMyInfo());
+        assertEquals("\nName: Emily Chen\n" +
+                "Age: 32\n" +
+                "Height: 169.00 cm\n" +
+                "Weight: 50.00 kg\n" +
+                "BMI: 17.51\n" +
+                "Birth date: 5/32/1991", personC.showMyInfo());
+        assertEquals("\nName: Elle CX\n" +
+                "Age: 30\n" +
+                "Height: 168.00 cm\n" +
+                "Weight: 50.00 kg\n" +
+                "BMI: 17.72\n" +
+                "Birth date: 2/29/1993", personD.showMyInfo());
+        assertEquals("\nName: Ashley CA\n" +
+                "Age: 31\n" +
+                "Height: 168.00 cm\n" +
+                "Weight: 50.00 kg\n" +
+                "BMI: 17.72\n" +
+                "Birth date: 2/29/1992", personE.showMyInfo());
+
+        System.out.println("[v] showMyInfo test passed");
+    }
+
+    @Test
+    void nameCheck() {
+        System.out.println("[+] Here is nameCheck test");
+        System.out.println("To testing the nameCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.nameCheck("");
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is empty, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] nameCheck test passed");
+    }
+
+    @Test
+    void yearCheck() {
+        System.out.println("[+] Here is yearCheck test");
+        System.out.println("To testing the yearCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.yearCheck(1849);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is 1849, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.yearCheck(2027);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Input is 2027, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] yearCheck test passed");
+    }
+
+    @Test
+    void monthCheck() {
+        System.out.println("[+] Here is monthCheck test");
+        System.out.println("To testing the monthCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.monthCheck(0, 2000);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is 0, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.monthCheck(13, 2000);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Input is 13, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] monthCheck test passed");
+    }
+
+    @Test
+    void isLeapYear() {
+        System.out.println("[+] Here is isLeapYear test");
+        System.out.println("To testing the isLeapYear function");
+
+        assertEquals(false, person.isLeapYear(2001));
+        assertEquals(true, person.isLeapYear(2000));
+        assertEquals(false, person.isLeapYear(1900));
+        assertEquals(true, person.isLeapYear(2004));
+        assertEquals(false, person.isLeapYear(2005));
+
+        assertEquals(false, personD.isLeapYear(personD.yearOfBirth));
+        assertEquals(true, personE.isLeapYear(personE.yearOfBirth));
+
+        System.out.println("[v] isLeapYear test passed");
+    }
+
+    @Test
+    void dayCheck() {
+        System.out.println("[+] Here is dayCheck test");
+        System.out.println("To testing the dayCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.dayCheck(1, 0, 2000);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Date: " + person.getBirthDate());
+        System.out.println("Input is 0, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.dayCheck(1, 32, 2000);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Date: " + person.getBirthDate());
+        System.out.println("Input is 32, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.dayCheck(2, 29, 2001);
+        });
+        System.out.println("Case #3: pass");
+        System.out.println("Date: " + person.getBirthDate());
+        System.out.println("Input is 29, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.dayCheck(2, 30, 2000);
+        });
+        System.out.println("Case #4: pass");
+        System.out.println("Date: " + person.getBirthDate());
+        System.out.println("Input is 30, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] dayCheck test passed");
+    }
+
+    @Test
+    void heightCheck() {
+        System.out.println("[+] Here is heightCheck test");
+        System.out.println("To testing the heightCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.heightCheck(0);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is 0, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.heightCheck(1.68);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Input is 1.68, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] heightCheck test passed");
+    }
+
+    @Test
+    void weightCheck() {
+        System.out.println("[+] Here is weightCheck test");
+        System.out.println("To testing the weightCheck function");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            person.weightCheck(0);
+        });
+        System.out.println("Case #1: pass");
+        System.out.println("Input is 0, " + "Exception message: " + exception.getMessage());
+
+        exception = assertThrows(Exception.class, () -> {
+            person.weightCheck(900);
+        });
+        System.out.println("Case #2: pass");
+        System.out.println("Input is 900, " + "Exception message: " + exception.getMessage());
+
+        System.out.println("[v] weightCheck test passed");
+    }
+
+    @Test
+    void main() {
+    }
 }
