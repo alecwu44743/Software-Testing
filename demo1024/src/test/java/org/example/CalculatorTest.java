6ec35bdf1dfd97b1d3b50e836208f236243e1783
alecwu44743 <alec.wu44743@gmail.com>
2023-10-24 17:09:17 +0800

    on course. demo1024 ParameterizedTest

--- demo1024/src/test/java/org/example/CalculatorTest.java
+++ demo1024/src/test/java/org/example/CalculatorTest.java
@@ -1,16 +1,51 @@
 package org.example;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
-    void addTest(int v) {
+    void addOne(int v) {
         Calculator c = new Calculator();
         assertEquals(v + 1, c.add(v, 1));
+    }
+
+    @ParameterizedTest
+    @NullSource // -> 1
+    @EmptySource // -> 2
+    @ValueSource(strings = { " ", "   ", "\t", "\n" }) // -> 3-6
+    void nullEmptyAndBlankStrings(String text) {
+        assertTrue(text == null || text.trim().isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "apple,         1",
+            "banana,        2",
+            "'lemon, lime', 0xF1",
+            "strawberry,    700_000"
+    })
+    void testWithCsvSource(String fruit, int rank) {
+        assertNotNull(fruit);
+        assertNotEquals(0, rank);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3",
+            "-1, 1, 0",
+            "1, -1, 0",
+            "-1, -2, -3"
+    })
+    void addTest(int a, int b, int c) {
+        Calculator calculator = new Calculator();
+        assertEquals(c, calculator.add(a, b));
     }
 }
