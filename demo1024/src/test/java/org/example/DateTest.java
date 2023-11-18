03690fabb7998009dcca5781588ece010b83ca99
alecwu44743 <alec.wu44743@gmail.com>
2023-11-19 01:05:18 +0800

    fix. converage test

--- demo1024/src/test/java/org/example/DateTest.java
+++ demo1024/src/test/java/org/example/DateTest.java
@@ -7,15 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class DateTest {
 
     @Test
-    void getYear() {
-    }
-
-    @Test
-    void getMonth() {
-    }
-
-    @Test
-    void getDay() {
+    void testTommorow1() {
+        Date date = new Date(2020, 12, 20);
+        Date exp = new Date(2020, 12, 21);
+        assertEquals(exp.toString(), date.tomorrow().toString());
     }
 
     @Test
@@ -26,6 +21,44 @@ class DateTest {
     }
 
     @Test
-    void testToString() {
+    void testLeapYear(){
+        Date date = new Date(2020, 2, 28);
+        Date exp = new Date(2020, 2, 29);
+        assertEquals(exp.toString(), date.tomorrow().toString());
+    }
+
+    @Test
+    void testNewMonth(){
+        Date date = new Date(2020, 1, 31);
+        Date exp = new Date(2020, 2, 1);
+        assertEquals(exp.toString(), date.tomorrow().toString());
+    }
+
+    @Test
+    void testNewYear(){
+        Date date = new Date(2020, 12, 31);
+        Date exp = new Date(2021, 1, 1);
+        assertEquals(exp.toString(), date.tomorrow().toString());
+    }
+
+    @Test
+    void testGetYear(){
+        Date date = new Date(2020, 12, 31);
+        Date exp = new Date(2020, 12, 31);
+        assertEquals(exp.getYear(), date.getYear());
+    }
+
+    @Test
+    void testGetMonth(){
+        Date date = new Date(2020, 12, 31);
+        Date exp = new Date(2020, 12, 31);
+        assertEquals(exp.getMonth(), date.getMonth());
+    }
+
+    @Test
+    void testGetDay(){
+        Date date = new Date(2020, 12, 31);
+        Date exp = new Date(2020, 12, 31);
+        assertEquals(exp.getDay(), date.getDay());
     }
 }
