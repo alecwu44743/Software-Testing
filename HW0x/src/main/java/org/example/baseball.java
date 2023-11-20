a25c4f7ba889ede95cf222f44cf00b4e24587800
alecwu44743 <alec.wu44743@gmail.com>
2023-11-20 23:19:32 +0800

    comment

--- HW0x/src/main/java/org/example/baseball.java
+++ HW0x/src/main/java/org/example/baseball.java
@@ -4,39 +4,43 @@ import java.util.Arrays;
 
 public class Baseball {
     public void validateArray(int inningA[], int inningB[], int playerA[], int playerB[]) throws Exception{
-        if(inningA.length < 9 || inningB.length < 9) {
+        if(inningA.length < 9 || inningB.length < 9) { // inning length shouldn't be less than 9
             throw new Exception("局數小於九局");
         }
 
-        if(inningA.length != inningB.length) {
+        if(inningA.length != inningB.length) { // inning length should be equal
             throw new Exception("局數不一致");
         }
 
+        // total score of inning should be equal to total score of player
         if((calculateArray(inningA) != calculateArray(playerA)) || (calculateArray(inningB) != calculateArray(playerB))) {
             throw new Exception("總分不一致");
         }
 
-        if(calculateArray(inningA) == calculateArray(inningB)) {
+        if(calculateArray(inningA) == calculateArray(inningB)) { // total score of inning should not be equal
             throw new Exception("不可以和局");
         }
 
+        // if inningB[8] is -1, 
+        // the total score of the first 8 innings of inningB should be 
+        // greater than the total score of the first 9 innings of inningA
         if(inningB[8] == -1 && !(calculateArray(Arrays.copyOfRange(inningA, 0, 9)) < calculateArray(Arrays.copyOfRange(inningB, 0, 8)))) {
             throw new Exception("不合理的提前結束");
         }
 
         for(int i=0; i<inningB.length; i++) {
-            if((inningB[i] == -1 && i != 8) || inningA[i] == -1) {
+            if((inningB[i] == -1 && i != 8) || inningA[i] == -1) { // inningB[8] should be the only -1 in inningB
                 throw new Exception("提前結束只可能出現在九下");
             }
         }
 
-        if ((calculateArray(Arrays.copyOfRange(inningA, 0, 9)) != calculateArray(Arrays.copyOfRange(inningB, 0, 9))) && (inningA.length > 9 || inningB.length > 9)) {
+        if ((calculateArray(Arrays.copyOfRange(inningA, 0, 9)) != calculateArray(Arrays.copyOfRange(inningB, 0, 9))) && (inningA.length > 9 || inningB.length > 9)) { // should not have extra innings
             throw new Exception("沒有必要的延長局");
         }
 
     }
 
-    public int calculateArray(int input[]){
+    public int calculateArray(int input[]){ // calculate total score of an array
         int sum = 0;
         for (int i = 0; i < input.length; i++) {
             if(input[i] == -1){
@@ -47,9 +51,13 @@ public class Baseball {
         return sum;
     }
 
+    // calculate the score of the game
     public int score(int inningA[], int inningB[], int playerA[], int playerB[]) throws Exception {
-        validateArray(inningA, inningB, playerA, playerB);
+        validateArray(inningA, inningB, playerA, playerB); // validate the input arrays
 
+        // return the score of the game
+        // positive number means playerA wins
+        // negative number means playerB wins
         return calculateArray(inningA) - calculateArray(inningB);
     }
 }
--- HW0x/src/test/java/org/example/baseballTest.java
+++ HW0x/src/test/java/org/example/baseballTest.java
@@ -89,18 +89,6 @@ class BaseballTest {
     public void testScore_5() throws Exception {
         Baseball game = new Baseball();
 
-        int[] inningA = {1,1,1,1,1,1,2,-1,0};
-        int[] inningB = {1,1,1,1,1,1,1,0,0};
-        int[] playerA = {1,1,1,1,1,1,2,0,0};
-        int[] playerB = {1,1,1,1,1,1,1,0,0};
-
-        assertThrows(Exception.class, () -> game.score(inningA, inningB, playerA, playerB));
-    }
-
-    @Test // Example: Test for "不合理的提前結束" exception
-    public void testScore_6() throws Exception {
-        Baseball game = new Baseball();
-
         // Test case where inningA[7] is -1, and the total score of the first 7 innings is less than or equal to the total score of the first 8 innings of inningB
         int[] inningA = {1, 1, 1, 1, 1, 1, 1, 1, 1};
         int[] inningB = {1, 1, 1, 1, 1, 1, 1, -1, 0};
@@ -111,7 +99,7 @@ class BaseballTest {
     }
 
     @Test // Example: Test for "不合理的提前結束" exception
-    public void testScore_7() throws Exception {
+    public void testScore_6() throws Exception {
         Baseball game = new Baseball();
 
         // Test case where inningA[7] is -1, and the total score of the first 7 innings is greater than the total score of the first 8 innings of inningB
@@ -124,7 +112,7 @@ class BaseballTest {
     }
 
     @Test // Example: Test for "不合理的提前結束" exception
-    public void testScore_8() throws Exception {
+    public void testScore_7() throws Exception {
         Baseball game = new Baseball();
 
         int[] inningA = {1, 1, 1, 1, 1, 1, 1, 1, 1};
@@ -136,7 +124,7 @@ class BaseballTest {
     }
 
     @Test // Example: Test for "沒有必要的延長局" exception
-    public void testScore_9() throws Exception {
+    public void testScore_8() throws Exception {
         Baseball game = new Baseball();
 
         // Test case where inningA[7] is -1, inningB[7] is 'X', and the total score of the first 7 innings of inningA is equal to the total score of the first 7 innings of inningB
@@ -149,7 +137,7 @@ class BaseballTest {
     }
 
     @Test // Example: Test for "提前結束只可能出現在九下" exception
-    public void testScore_10() throws Exception {
+    public void testScore_9() throws Exception {
         Baseball game = new Baseball();
 
         // Test case where inningA[7] is -1, and inningB[6] is -1
